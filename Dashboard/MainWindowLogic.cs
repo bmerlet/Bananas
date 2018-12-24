200bfc38f11af1e177ff9335623fff6db3849c2a
Benoit Merlet <merlet_benoit@yahoo.com>
2018-12-24 09:52:35 -0500

    Attempt at using ofx for investment dashboard. Not working

---
+++ Dashboard/MainWindowLogic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard
+{
+    public class MainWindowLogic
+    {
+        #region Constructor
+
+        private Listener listener = new Listener();
+
+        public MainWindowLogic(Action Exit)
+        {
+            InfoCommand = new UICommand(Info);
+            RefreshCommand = new UICommand(Refresh);
+            ExitCommand = new UICommand(Exit);
+        }
+
+        #endregion
+
+        #region UI properties
+
+        //
+        // Commands
+        //
+        public UICommand InfoCommand { get; }
+        public UICommand RefreshCommand { get; }
+        public UICommand ExitCommand { get; }
+
+        #endregion
+
+        #region Actions
+
+        private void Info()
+        {
+            var refreshManager = new RefreshManager();
+            refreshManager.GetFinancialInstitutionInfo();
+        }
+
+        private void Refresh()
+        {
+            var refreshManager = new RefreshManager();
+            refreshManager.Connect();
+            // ZZZ
+        }
+
+        #endregion
+    }
+}
---
+++ Dashboard/UICommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Dashboard
+{
+    /// <summary>
+    /// Basic implementation of xaml ICommand
+    /// </summary>
+    public class UICommand : ICommand
+    {
+        #region Private memebers
+
+        private bool canExecute;
+        private Action action;
+
+        #endregion
+
+        #region Actions from logic
+
+        public UICommand(Action action, bool canExecute = true)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public void SetCanExecute(bool value)
+        {
+            if (canExecute != value)
+            {
+                canExecute = value;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        #endregion
+
+        #region ICommand implementation
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        #endregion
+    }
+}
