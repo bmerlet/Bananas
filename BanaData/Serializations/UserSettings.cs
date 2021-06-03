12328e81da310db54ffd1607a5716c2b3b163e39
bmerlet <merlet_benoit@yahoo.com>
2021-06-03 14:19:34 -0400

    Column width defs in user settings

--- BanaData/Serializations/UserSettings.cs
+++ BanaData/Serializations/UserSettings.cs
@@ -23,5 +23,16 @@ namespace BanaData.Serializations
 
         // Last file opened
         public string LastFileOpened;
+
+        // Column widths
+        public double WidthOfDateColumn = 80;
+        public double WidthOfMediumColumn = 80;
+        public double WidthOfPayeeColumn = 140;
+        public double WidthOfMemoColumn = 140;
+        public double WidthOfCategoryColumn = 140;
+        public double WidthOfPaymentColumn = 90;
+        public double WidthOfStatusColumn = 40;
+        public double WidthOfDepositColumn = 90;
+        public double WidthOfBalanceColumn = 90;
     }
 }
